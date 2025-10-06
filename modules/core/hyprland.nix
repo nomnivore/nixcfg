0aa2589e8dec0814523792b3bcc28b46bba59c57
nomnivore <6979410+nomnivore@users.noreply.github.com>
2025-10-06 17:58:43 -0400

    barebones hyprland module

---
+++ modules/core/hyprland.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+{
+  programs.hyprland.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    kitty # required for default hyprland config
+  ];
+
+  # hint Electron apps to use Wayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+}
