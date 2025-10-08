f0f42fdb8cd639e19d248632fc360b94a2a53883
nomnivore <6979410+nomnivore@users.noreply.github.com>
2025-10-08 13:42:32 -0400

    add better hardware config

--- hosts/vbox/default.nix
+++ hosts/vbox/default.nix
@@ -37,10 +37,24 @@
 
   security.sudo.wheelNeedsPassword = false;
 
+  # following the setup described here to create 3 partitions for UEFI
+  # https://nixos.org/manual/nixos/stable/#sec-installation-manual-partitioning-UEFI
+  # TODO: consider choosing devices by-label
   fileSystems."/" = {
     device = "/dev/sda1";
     fsType = "ext4";
   };
+  fileSystems."/boot" = {
+    device = "/dev/sda3";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
+  swapDevices = [
+    { device = "/dev/sda2"; }
+  ];
 
   # bootloader
   boot.loader.limine = {
