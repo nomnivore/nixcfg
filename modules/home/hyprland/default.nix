bdd5fb06903e434f2dfda9f63dcbad5b25faed98
nomnivore <6979410+nomnivore@users.noreply.github.com>
2025-10-13 22:31:03 -0400

    multimedia key bindings

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -24,7 +24,6 @@ with lib;
     [ ]
     # conditional imports
     ++ (optionals (enable) [
-      ./swayosd.nix # OSD for media keys
       ./waybar
       ./hyprpaper.nix # wallpaper
     ]);
@@ -32,6 +31,8 @@ with lib;
 
     # some default apps referenced in config
     programs.foot.enable = true;
+    services.swayosd.enable = true;
+    services.playerctld.enable = true;
 
     wayland.windowManager.hyprland = {
       enable = true;
@@ -52,6 +53,8 @@ with lib;
         "$terminal" = "foot";
         # "$fileManager" = "dolphin";
         "$menu" = "wofi --term=$terminal --show drun";
+        "$osdclient" =
+          "swayosd-client --monitor \"$(hyprctl monitors -j | jq -r '.[] | select(.focused == true).name')";
 
         # AUTOSTART
         exec-once = [
@@ -209,6 +212,30 @@ with lib;
           "$mod, mouse_up, workspace, e-1"
 
         ];
+        # laptop multimedia keys
+        bindeld = [
+          ",XF86AudioRaiseVolume, Volume Up, exec, $osdclient --output-volume raise"
+          ",XF86AudioLowerVolume, Volume down, exec, $osdclient --output-volume lower"
+          ",XF86AudioMute, Mute, exec, $osdclient --output-volume mute-toggle"
+          ",XF86AudioMicMute, Mute microphone, exec, $osdclient --input-volume mute-toggle"
+          ",XF86MonBrightnessUp, Brightness up, exec, $osdclient --brightness raise"
+          ",XF86MonBrightnessDown, Brightness down, exec, $osdclient --brightness lower"
+
+          # 1% adjustments with alt modifier
+
+          "ALT, XF86AudioRaiseVolume, Volume Up precise, exec, $osdclient --output-volume +1"
+          "ALT, XF86AudioLowerVolume, Volume down precise, exec, $osdclient --output-volume -1"
+          "ALT, XF86MonBrightnessUp, Brightness up precise, exec, $osdclient --brightness +1"
+          "ALT, XF86MonBrightnessDown, Brightness down precise, exec, $osdclient --brightness -1"
+        ];
+
+        bindld = [
+          ",XF86AudioNext, Next track, exec, $osdclient --playerctl next"
+          ",XF86AudioPause, Pause, exec, $osdclient --playerctl play-pause"
+          ",XF86AudioPlay, Play, exec, $osdclient --playerctl play-pause"
+          ",XF86AudioPrev, Previous track, exec, $osdclient --playerctl previous"
+        ];
+
       };
     };
 
--- modules/home/hyprland/swayosd.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }:
-
-{
-  services.swayosd.enable = true;
-  # hyprland will automatically start this, no further configuration needed
-}
