5e934a78067b4858ceae4df4bb3c05e84254b34f
nomnivore <6979410+nomnivore@users.noreply.github.com>
2025-10-16 01:52:20 -0400

    fix path relativity

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -3,21 +3,21 @@
 {
   imports = [
     # /// Always-on modules ///
-    ../modules/core/nix.nix
-    ../modules/core/nix-ld.nix
-    ../modules/core/nh.nix
-    # default user
-    ../modules/core/user.nix
-    # set shell to zsh
-    ../modules/core/zsh.nix
+    ./nix.nix
 
+    ./nix-ld.nix
+    ./nh.nix
+    # default user
+    ./user.nix
+    # set shell to zsh
+    ./zsh.nix
 
     # /// Optional modules ///
     # // off by default //
     # styles
-    ../../modules/core/stylix.nix
+    ./stylix.nix
     # desktops
-    ../../modules/core/hyprland.nix
-    ../../modules/core/cosmic.nix
+    ./hyprland.nix
+    ./cosmic.nix
   ];
 }
