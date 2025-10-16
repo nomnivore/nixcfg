bcdddb908f32b84eb78053dce05e2845db05ef4b
nomnivore <6979410+nomnivore@users.noreply.github.com>
2025-10-16 15:10:35 -0400

    fix conditional stylix module logic

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -2,21 +2,27 @@
   pkgs,
   lib,
   osConfig,
+  config,
   ...
 }:
 
 with lib;
 {
-  config = mkIf (osConfig.modules.stylix.enable) {
-    stylix.targets = {
-      waybar.addCss = false; # colors/fonts only
+  config =
+    { }
+    // optionalAttrs osConfig.modules.stylix.enable {
+      # 'stylix' does not exist in home-manager unless the NixOS module is enabled
+      stylix = {
+        targets = {
+          waybar.addCss = false; # colors/fonts only
 
-      wezterm.enable = false;
-      neovim.enable = false;
-      vscode.enable = false;
+          wezterm.enable = false;
+          neovim.enable = false;
+          vscode.enable = false;
+        };
+
+        # user fonts
+        fonts.fontconfig.enable = true;
+      };
     };
-
-    # user fonts
-    fonts.fontconfig.enable = true;
-  };
 }
