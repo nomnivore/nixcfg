08d8ca97bbb17162008df336266f972e1cf80eac
nomnivore <6979410+nomnivore@users.noreply.github.com>
2025-10-21 16:47:12 -0400

    limine styling and timeout

--- modules/core/limine.nix
+++ modules/core/limine.nix
@@ -6,6 +6,7 @@
 }:
 let
   cfg = config.modules.limine;
+  colors = config.lib.stylix.colors;
 in
 with lib;
 {
@@ -20,6 +21,27 @@ with lib;
       enable = true;
       efiSupport = true;
       biosSupport = false;
+
+      style = {
+        wallpapers = [ ]; # unset default wallpaper
+        interface.branding = " "; # title at the top of the screen
+
+        graphicalTerminal = {
+          palette = "${colors.base00};${colors.base08};${colors.base0B};${colors.base0A};${colors.base0D};${colors.base0E};${colors.base0C};${colors.base05}";
+          brightPalette = "${colors.base04};${colors.base08};${colors.base0B};${colors.base0A};${colors.base0D};${colors.base0E};${colors.base0C};${colors.base05}";
+          background = colors.base00;
+          foreground = colors.base05;
+          brightBackground = colors.base04;
+          brightForeground = colors.base05;
+        };
+      };
+
+      # quickly boot default/most recent generation
+      # mash a key to interrupt this on startup
+      # except: s, space, enter, esc
+      extraConfig = ''
+        timeout: 1
+      '';
     };
   };
 }
