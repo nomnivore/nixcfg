ecd2782797d481a70e5916596917b9277ffa997e
nomnivore <6979410+nomnivore@users.noreply.github.com>
2025-10-21 15:29:31 -0400

    add cachix caches

--- flake.nix
+++ flake.nix
@@ -152,4 +152,16 @@
         modules = [ ./users/kyle/home.nix ];
       };
     };
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://statix.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "statix.cachix.org-1:Z9E/g1YjCjU117QOOt07OjhljCoRZddiAm4VVESvais="
+    ];
+  };
 }
