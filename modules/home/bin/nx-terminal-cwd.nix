4f56b3a96ad04ac62aa03ba050cc204ae3c64b48
nomnivore <6979410+nomnivore@users.noreply.github.com>
2025-10-29 21:24:31 -0400

    use -1 if hypr is not running

--- modules/home/bin/nx-terminal-cwd.nix
+++ modules/home/bin/nx-terminal-cwd.nix
@@ -6,20 +6,27 @@
   ...
 }:
 let
+  enabled = config.modules.nx.isDesktop;
   isHypr = config.modules.hyprland.enable;
-in
-pkgs.writeShellScriptBin "nx-terminal-cwd" ''
-  terminal_pid=$(hyprctl activewindow | awk '/pid:/ {print $2}')
-  shell_pid=$(pgrep -P "$terminal_pid" | tail -n1)
 
-  if [[ -n $shell_pid ]]; then
-    cwd=$(readlink -f "/proc/$shell_pid/cwd" 2>/dev/null)
-    if [[ -n $cwd ]]; then
-      echo "$cwd"
+  term_pid_cmd = if isHypr then ''$(hyprctl activewindow | awk '/pid:/ {print $2}')'' else ''"-1"'';
+in
+if enabled then
+  pkgs.writeShellScriptBin "nx-terminal-cwd" ''
+    terminal_pid=${term_pid_cmd}
+    shell_pid=$(pgrep -P "$terminal_pid" | tail -n1)
+
+    if [[ -n $shell_pid ]]; then
+      cwd=$(readlink -f "/proc/$shell_pid/cwd" 2>/dev/null)
+      if [[ -n $cwd ]]; then
+        echo "$cwd"
+      else
+        echo "$HOME"
+      fi
     else
       echo "$HOME"
     fi
-  else
-    echo "$HOME"
-  fi
-''
+  ''
+else
+  null
+
