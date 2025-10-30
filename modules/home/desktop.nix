4687dbaa695f6dd588bf03363df816660e49be23
nomnivore <6979410+nomnivore@users.noreply.github.com>
2025-10-29 20:47:43 -0400

    add github web app

--- modules/home/desktop.nix
+++ modules/home/desktop.nix
@@ -34,6 +34,7 @@ with lib;
     ];
 
     # web apps
+    # icons from https://dashboardicons.com/
     xdg.desktopEntries = {
       gemini = {
         name = "Gemini";
@@ -41,6 +42,16 @@ with lib;
         icon = pkgs.fetchurl {
           url = "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-gemini.png";
           sha256 = "sha256-U16hXdmDWtsO9308rxhnmHBQMMe0QO7mySL2wMugBiM=";
+        };
+        type = "Application";
+      };
+
+      github = {
+        name = "GitHub";
+        exec = "nx-launch-webapp https://github.com";
+        icon = pkgs.fetchurl {
+          url = "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png";
+          sha256 = "sha256-BFLUsd6GVtiudyS5M5JGtRLdvO/5G5mjYVp05zW7x6o=";
         };
         type = "Application";
       };
