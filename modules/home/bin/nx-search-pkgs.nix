00e19c7beae326d71dc2334c09c76a251c9453a8
nomnivore <6979410+nomnivore@users.noreply.github.com>
2025-11-14 01:13:07 -0500

    fix build for nx-search-pkgs

--- modules/home/bin/nx-search-pkgs.nix
+++ modules/home/bin/nx-search-pkgs.nix
@@ -3,32 +3,14 @@
   ...
 }:
 
-pkgs.writeShellScriptBin {
+let
+  content = builtins.readFile ./nx-search-pkgs;
+in
+pkgs.writeShellApplication {
   name = "nx-search-pkgs";
   runtimeInputs = with pkgs; [
     fzf
     unstable.nix-search-cli
   ];
-  text = ''
-    SEARCH_CMD='nix-search --json "$QUERY" -m 40 | jq -s -r "map(select(.type == \"package\") | .package_pname)[]" | uniq'
-
-    RELOAD_CMD="sh -c 'QUERY={q}; [ -z \"\$QUERY\" ] && echo \"\" || $SEARCH_CMD'"
-
-    fzf_args=(
-      --multi
-      --query ""
-      --bind "change:reload:$RELOAD_CMD"
-      --phony
-      --preview 'nix-search --name {1} -m 1 --details'
-      --preview-window 'down:35%:wrap'
-      --bind 'alt-p:toggle-preview'
-      --color 'preview-fg:-1'
-      --preview-label $'Search for Nix packages (Type to search, ENTER to confirm)'
-    )
-
-    # 3. Initial command for fzf's startup (pipes an empty line to start with no results).
-    pkgs=$(echo "" | fzf "$\{fzf_args[@]}" | xargs)
-
-    exec ns $pkgs
-  '';
+  text = content;
 }
