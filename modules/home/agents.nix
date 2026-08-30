a91301561c6dd26fa2e7f1e498c71bc2f39a5c52
nomnivore <6979410+nomnivore@users.noreply.github.com>
2026-08-30 03:43:00 -0400

    add skills mirror

--- modules/home/agents.nix
+++ modules/home/agents.nix
@@ -9,6 +9,9 @@
   modules.extRepos.agents = {
     repo = "nomnivore/agents";
     links = {
+      # TODO: generalize (sub)agents and AGENTS.md
+      ".agents/skills" = "skills";
+
       ".claude/CLAUDE.md" = "CLAUDE.md";
       ".claude/skills" = "skills";
       ".claude/agents" = "agents";
