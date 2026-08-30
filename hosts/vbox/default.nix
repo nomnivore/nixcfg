445ef621f7a15bc673c4c652e13181dbf5c961dd
nomnivore <6979410+nomnivore@users.noreply.github.com>
2026-08-30 03:42:51 -0400

    remove invalid option

--- hosts/vbox/default.nix
+++ hosts/vbox/default.nix
@@ -44,8 +44,6 @@ in
         {
           modules = sharedModules // {
             # my home options
-            userDirs.enable = true;
-
             walker.enable = true;
           };
         }
